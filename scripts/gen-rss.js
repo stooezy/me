a348bf1ff2ac9f381fbd4356858b4528558522a9
stooezy <114982052+stooezy@users.noreply.github.com>
2023-08-29 11:42:26 +0700

    Update gen-rss.js

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Yoga Permana',
+    site_url: 'https://dymple.id',
+    feed_url: 'https://dymple.id/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
